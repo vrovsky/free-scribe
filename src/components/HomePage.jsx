f4fc6da5761e17d8118c8e9cabccc1bdeabcb7fb
Viachaslau Rouski <vrovsky@gmail.com>
2025-05-14 14:16:14 +0100

    implemented stop and start recording functions

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function HomePage(props) {
   const { setFile, setAudioStream } = props;
+
+  const [recordingStatus, setRacordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+
+  const mediaRecorder = useRef(null);
+  const mimeType = "audio/webm";
+
+  async function startRecording() {
+    let tempStream;
+
+    console.log("Start recording...");
+    try {
+      const streamData = navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      tempStream = streamData;
+    } catch (err) {
+      console.log(err.message);
+      return;
+    }
+    setRacordingStatus("recording");
+
+    //create new media recorder instanse using the stream
+    const media = new MediaRecorder(tempStream, {
+      type: mimeType,
+    });
+
+    mediaRecorder.current = media;
+    mediaRecorder.current.start();
+    let localAudioChunk = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localAudioChunk.push(event.data);
+    };
+    setAudioChunks(localAudioChunk);
+  }
+
+  async function stopRecording() {
+    setRacordingStatus("inactive");
+    console.log("Stop recording");
+
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, {
+        type: mimeType,
+      });
+      setAudioStream(audioBlob);
+      setAudioChunks([]);
+    };
+  }
+
   return (
     <main className="flex-1 p-4 flex flex-col text-center gap-3 sm:gap-4 md:gap-5 justify-center pb-20">
       <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
